bf845691fdb445fca4370b3388462403914a3b4d
charlottelai18 <charlottel2015@gmail.com>
2025-12-22 16:11:30 +1100

    cleaning up code

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ let wordStore = [];
 let wordMutate = [];
 let incorrectGuessCount = 0;
 
+//QUERY SELECTORS
+const wordDisplay = document.querySelector(".game--word--display");
+const guessedBtns = document.querySelectorAll(".game--letter");
+guessedBtns.forEach((btn) => {btn.addEventListener("click", checkGuessedLetter)});
+const hangmanImage = document.querySelector(".game--img--status");
+const gameResults = document.querySelector(".game--results")
+const newGameBtn = document.querySelector(".game--new--game");
+newGameBtn.addEventListener("click", startNewGame);
+
 // RETRIEVING NEW WORD USING RANDOM WORD API
 async function getWord() {
   const response = await fetch(
@@ -19,11 +28,7 @@ async function startNewGame() {
   resetData();
   mutatingWord();
   displayHangmanWord();
-  console.log("New word:", word);
 }
-
-const newGameBtn = document.querySelector(".game--new--game");
-newGameBtn.addEventListener("click", startNewGame);
 
 function mutatingWord(){
     for (let i = 0; i < wordStore.length; i++){
@@ -32,19 +37,13 @@ function mutatingWord(){
 }
 
 //DISPLAYNG THE NEW WORD USING ___ ON THE UI
-const wordDisplay = document.querySelector(".game--word--display");
 function displayHangmanWord(){
     wordDisplay.innerHTML = wordMutate.join(" ");
 }
 
 //GUESSING LETTERS
-const guessedBtns = document.querySelectorAll(".game--letter");
-guessedBtns.forEach((btn) => {btn.addEventListener("click", checkGuessedLetter)});
-const hangmanImage = document.querySelector(".game--img--status");
-
 function checkGuessedLetter(event) {
   const guessedLetter = event.target.innerText.toLowerCase();
-  console.log("User guessed:", guessedLetter);
 
   guessedLetters.push(guessedLetter);
 
@@ -56,19 +55,13 @@ function checkGuessedLetter(event) {
     }
     displayHangmanWord();
     event.target.disabled = true;
-    console.log(guessedLetter + " is in the word!");
   } else {
     incorrectGuessCount += 1;
     hangmanImage.src = `imgs/h-${incorrectGuessCount}.jpg`
-    console.log(guessedLetter + " is not in the word");
   }
   checkIfWon();
-  console.log(guessedLetters)
-  console.log(wordStore)
-  console.log(wordMutate)
 }
 
-const gameResults = document.querySelector(".game--results")
 function checkIfWon(){
     if (incorrectGuessCount === 10){
         gameResults.innerHTML = "YOU LOST TRY AGAIN"
