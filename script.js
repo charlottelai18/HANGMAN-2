420372caee1520c7142e02709ea86036d834ef3e
charlottelai18 <charlottel2015@gmail.com>
2025-12-22 15:13:13 +1100

    adding win condition and changing images according to incorrect guesses

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let guessedLetters = [];
 let wordStore = [];
 let wordMutate = [];
+let incorrectGuessCount = 0;
 
 // RETRIEVING NEW WORD USING RANDOM WORD API
 async function getWord() {
@@ -15,6 +16,7 @@ async function getWord() {
 async function startNewGame() {
   const word = await getWord();
   wordStore = word.split('')
+  resetData();
   mutatingWord();
   displayHangmanWord();
   console.log("New word:", word);
@@ -38,24 +40,51 @@ function displayHangmanWord(){
 //GUESSING LETTERS
 const guessedBtns = document.querySelectorAll(".game--letter");
 guessedBtns.forEach((btn) => {btn.addEventListener("click", checkGuessedLetter)});
-
+const hangmanImage = document.querySelector(".game--img--status");
 
 function checkGuessedLetter(event) {
   const guessedLetter = event.target.innerText.toLowerCase();
   console.log("User guessed:", guessedLetter);
 
+  guessedLetters.push(guessedLetter);
+
   if (wordStore.includes(guessedLetter)){
-    guessedLetters.push(guessedLetter);
-    
     for (let i = 0; i < wordStore.length; i++){
         if (wordStore[i] === guessedLetter){
             wordMutate[i] = guessedLetter;
         }
     }
-    
     displayHangmanWord();
+    event.target.disabled = true;
     console.log(guessedLetter + " is in the word!");
   } else {
+    incorrectGuessCount += 1;
+    hangmanImage.src = `imgs/h-${incorrectGuessCount}.jpg`
     console.log(guessedLetter + " is not in the word");
   }
+  checkIfWon();
+  console.log(guessedLetters)
+  console.log(wordStore)
+  console.log(wordMutate)
+}
+
+const gameResults = document.querySelector(".game--results")
+function checkIfWon(){
+    if (incorrectGuessCount === 10){
+        gameResults.innerHTML = "YOU LOST TRY AGAIN"
+        incorrectGuessCount = 0;
+        guessedBtns.forEach((btn) => {btn.disabled = true});
+    }
+    else if (incorrectGuessCount < 10 && !wordMutate.includes("_")){
+        gameResults.innerHTML = "YAY U WON"
+    }
+}
+
+function resetData(){
+    guessedLetters = [];
+    wordMutate = [];
+    incorrectGuessCount = 0;
+    gameResults.innerHTML = "";
+    hangmanImage.src = `imgs/h-${0}.jpg`
+    guessedBtns.forEach((btn) => {btn.disabled = false});
 }
