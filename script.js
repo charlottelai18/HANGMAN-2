c1fbd053b290dd88fa22db2bb65937ad683226f6
charlottelai18 <charlottel2015@gmail.com>
2025-12-22 13:19:31 +1100

    displaying the hidden word on the UI

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//
+// RETRIEVING NEW WORD USING RANDOM WORD API
+//
 async function getWord() {
   const response = await fetch(
     "https://random-word-api.herokuapp.com/word?number=1"
@@ -8,8 +11,19 @@ async function getWord() {
 
 async function startNewGame() {
   const word = await getWord();
+  displayHangmanWord(word);
   console.log("New word:", word);
 }
 
 const newGameBtn = document.querySelector(".game--new--game");
 newGameBtn.addEventListener("click", startNewGame);
+
+//DISPLAYNG THE NEW WORD USING ___ ON THE UI
+const wordDisplay = document.querySelector(".game--word--display");
+function displayHangmanWord(word){
+    wordDisplay.innerHTML = "";
+
+    for (let i = 0; i < word.length; i++){
+        wordDisplay.innerHTML += "_ "
+    }
+}
