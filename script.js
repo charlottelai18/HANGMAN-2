2d53b1c221c79f6cc59ac691cda6b244701021fb
charlottelai18 <charlottel2015@gmail.com>
2025-12-22 14:26:34 +1100

    added logic for when the user guesses the letter correctly and changes UI based off that

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-//
+//DATA
+let guessedLetters = [];
+let wordStore = [];
+let wordMutate = [];
+
 // RETRIEVING NEW WORD USING RANDOM WORD API
-//
 async function getWord() {
   const response = await fetch(
     "https://random-word-api.herokuapp.com/word?number=1"
@@ -11,19 +14,48 @@ async function getWord() {
 
 async function startNewGame() {
   const word = await getWord();
-  displayHangmanWord(word);
+  wordStore = word.split('')
+  mutatingWord();
+  displayHangmanWord();
   console.log("New word:", word);
 }
 
 const newGameBtn = document.querySelector(".game--new--game");
 newGameBtn.addEventListener("click", startNewGame);
 
+function mutatingWord(){
+    for (let i = 0; i < wordStore.length; i++){
+        wordMutate[i] = "_";
+    }
+}
+
 //DISPLAYNG THE NEW WORD USING ___ ON THE UI
 const wordDisplay = document.querySelector(".game--word--display");
-function displayHangmanWord(word){
-    wordDisplay.innerHTML = "";
+function displayHangmanWord(){
+    wordDisplay.innerHTML = wordMutate.join(" ");
+}
 
-    for (let i = 0; i < word.length; i++){
-        wordDisplay.innerHTML += "_ "
+//GUESSING LETTERS
+const guessedBtns = document.querySelectorAll(".game--letter");
+guessedBtns.forEach((btn) => {btn.addEventListener("click", checkGuessedLetter)});
+
+
+function checkGuessedLetter(event) {
+  const guessedLetter = event.target.innerText.toLowerCase();
+  console.log("User guessed:", guessedLetter);
+
+  if (wordStore.includes(guessedLetter)){
+    guessedLetters.push(guessedLetter);
+    
+    for (let i = 0; i < wordStore.length; i++){
+        if (wordStore[i] === guessedLetter){
+            wordMutate[i] = guessedLetter;
+        }
     }
+    
+    displayHangmanWord();
+    console.log(guessedLetter + " is in the word!");
+  } else {
+    console.log(guessedLetter + " is not in the word");
+  }
 }
